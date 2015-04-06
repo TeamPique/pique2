ba9213fd4fb0a0e4088c665d8c4cb8966f5418c3
Evan Turner <evturn@gmail.com>
2015-04-06 17:13:26 -0400

    removed register template from view

--- app/assets/javascripts/views/surv.home.js
+++ app/assets/javascripts/views/surv.home.js
@@ -3,18 +3,12 @@ var surv = surv || {};
 surv.Home = Backbone.View.extend({
 	el: '#header',
 	surveyHeaderTemplate: _.template($('#survey-home-template').html()),
-	registerTemplate: _.template($('#register-template').html()),
 	initialize: function() {
 		this.survey();
 	},
 	survey: function() {
 		var survey = new surv.Survey({collection: questions});
 		this.$el.html(this.surveyHeaderTemplate());
-		return this;
-	},
-	register: function() {
-		$('#questionnaire').empty();
-		this.$el.html(this.registerTemplate());
 		return this;
 	},
 });
