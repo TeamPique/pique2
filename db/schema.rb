31327ceb5f1e38f81a9c3584967d040c8fabe29a
Evan Turner <evturn@gmail.com>
2015-04-06 15:42:03 -0400

    new questionnaire added to home view

--- db/schema.rb
+++ db/schema.rb
@@ -117,4 +117,4 @@ ActiveRecord::Schema.define(version: 20150403220355) do
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
-end
+
