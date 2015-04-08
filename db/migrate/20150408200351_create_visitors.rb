367ba8c4813050e5c2a9db263de0417b1315e059
Sean Jackson <sjackson44@gmail.com>
2015-04-08 16:11:13 -0400

    added vistors table

---
+++ db/migrate/20150408200351_create_visitors.rb
@@ -0,0 +1,11 @@
+class CreateVisitors < ActiveRecord::Migration
+  def change
+    create_table :visitors do |t|
+      t.integer :user_id
+      t.integer :visitor_id
+      t.datetime :date
+
+      t.timestamps null: false
+    end
+  end
+end
