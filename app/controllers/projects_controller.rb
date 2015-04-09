51d98c9484d57bb7ad12d0f0b0c7b84df5a44af7
Kamari Aykes <aykeskc@gmail.com>
2015-04-09 18:24:35 -0400

    adds new/create/params

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -8,4 +8,19 @@ class ProjectsController < ApplicationController
   def show
     @project = Project.find(params[:id])
   end
+
+  def new
+    @project = Project.new
+  end
+
+  def create
+    project = Project.create(projects_params)
+    project.save
+    redirect_to projects_path
+  end
+
+
+  def projects_params
+    params.permit(:name, :owner, :collaborators, :active, :number_of_collaborators, :about, :team, :openings, :case_studies)
+  end
 end
