ff991f99da94974619b9f6a5a8d9ff968f5c2411
Kristen MacFarlane <krismacfarlane@gmail.com>
2015-04-13 10:55:26 -0400

    adds search logic to the projects controller

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -5,6 +5,10 @@ class ProjectsController < ApplicationController
     @projects = Project.all
     @users = User.all
     @user = User.find_by(params[:id])
+
+    if params[:search]
+      @search_results = Project.search(params[:search]).order("created_at DESC")
+    end
   end
 
   def show
