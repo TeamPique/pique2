90dd5b610552fbf87e3d6ac2794edfa5f807dbda
Evan Turner <evturn@gmail.com>
2015-04-13 19:16:43 -0400

    recipent's avatar and name displays on sidebar

--- app/assets/javascripts/collections/conversation.js
+++ app/assets/javascripts/collections/conversation.js
@@ -4,13 +4,13 @@ var Conversation = Backbone.Collection.extend({
 		var x 			= this.length;
 		var model  	= this.models[x - 1];		
 		var content = model.get('content');
-		var sender 	= model.get('sender');
+		var recipient 	= model.get('recipient');
 		var time 		= model.get('timestamp');
-		var name 		= sender.get('name');
-		var avatar 	= sender.get('avatar');
+		var name 		= recipient.get('name');
+		var avatar 	= recipient.get('avatar');
 		var preview = new Message({
 				content: content,
-				sender: sender,
+				recipient: recipient,
 				timestamp: time,
 				username: name,
 				avatar: avatar
