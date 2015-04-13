020bf26c9d6d1b5e10119fa673745ec7d49266fe
Evan Turner <evturn@gmail.com>
2015-04-13 16:01:46 -0400

    sidebar template created

--- app/assets/javascripts/models/user.js
+++ app/assets/javascripts/models/user.js
@@ -2,5 +2,6 @@ var User = Backbone.Model.extend({});
 
 var tomJones = new User({
 	name: 'Tom Jones',
-	avatar: 'http://piqueapp.github.io/assets/img/av.png'
+	avatar: 'http://piqueapp.github.io/assets/img/av.png',
+	message: 'Craig, it\'s important. I just spilled salsa all over my filas.'
 });
--- app/assets/javascripts/views/message.js
+++ app/assets/javascripts/views/message.js
@@ -1,4 +1,11 @@
 var Message = Backbone.View.extend({
-	el: '.wrapper',
+	el: '.message-view',
 	sidebarMessageTpl: _.template($('#sidebar-message-template').html()),
+	initialize: function() {
+		this.render();
+	},
+	render: function() {
+		$('.sidebar-item').prepend(this.sidebarMessageTpl(tomJones.toJSON()));
+		return this;
+	},
 });
