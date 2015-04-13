2f455933d70db7746de6a9f936f888ad1c3e5f9c
Evan Turner <evturn@gmail.com>
2015-04-13 19:55:09 -0400

    template rendering to screen

--- app/assets/javascripts/collections/conversation.js
+++ app/assets/javascripts/collections/conversation.js
@@ -1,14 +1,14 @@
 var Conversation = Backbone.Collection.extend({
 	model: Message,
 	preview: function() {
-		var x 			= this.length;
-		var model  	= this.models[x - 1];		
-		var content = model.get('content');
-		var recipient 	= model.get('recipient');
-		var time 		= model.get('timestamp');
-		var name 		= recipient.get('name');
-		var avatar 	= recipient.get('avatar');
-		var preview = new Message({
+		var x 				= this.length;
+		var model  		= this.models[x - 1];		
+		var content 	= model.get('content');
+		var recipient = model.get('recipient');
+		var time 			= model.get('timestamp');
+		var name 			= recipient.get('name');
+		var avatar 		= recipient.get('avatar');
+		var preview 	= new Message({
 				content: content,
 				recipient: recipient,
 				timestamp: time,
--- app/assets/javascripts/views/sidebar.js
+++ app/assets/javascripts/views/sidebar.js
@@ -1,17 +1,32 @@
 var Sidebar = Backbone.View.extend({
 	el: '.message-view',
-	sidebarMessageTpl: _.template($('#sidebar-message-template').html()),
+	sidebarPreviewTpl: _.template($('#sidebar-message-template').html()),
+	chatMessageTpl	 : _.template($('#chat-message-template').html()),
 	initialize: function() {
+		moment().format();
+		var time = new Date();
 		this.render();
+	},
+	events: {
+		'keyup #message-input' : 'chat'
 	},
 	render: function() {
 		var preview = this.collection.preview();
-		$('#sidebar-items').prepend(this.sidebarMessageTpl(preview.toJSON()));
+		$('#sidebar-items').prepend(this.sidebarPreviewTpl(preview.toJSON()));
 		return this;
 	},
 	addAll: function() {
 		this.collection.each(function(model) {
 			this.addOne(model);
 		}.bind(this));
+	},
+	chat: function(e) {
+		if (e.keyCode == 13) {
+			var $chat = $('.message-chats');
+			$chat[0].scrollTop = $chat[0].scrollHeight;
+			$chat.append(this.chatMessageTpl());
+			$chat[0].scrollTop = $chat[0].scrollHeight;
+    	$(this).val('');
+  	}
 	},
 });
