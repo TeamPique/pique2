5ea8399079acd28bb72b57d331e78fd6ce130606
Evan Turner <evturn@gmail.com>
2015-04-20 14:15:08 -0400

    dropdown added to profile notifications

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,5 +1,29 @@
 var surv = surv || {};
 
+$(document).ready(function()
+{
+$("#notificationLink").click(function()
+{
+$("#notificationContainer").fadeToggle(300);
+$("#notification_count").fadeOut("slow");
+return false;
+});
+
+//Document Click
+$(document).click(function()
+{
+$("#notificationContainer").hide();
+});
+//Popup Click
+$("#notificationContainer").click(function()
+{
+return false
+});
+
+});
+
+
+
 var thomJones =
 		{
 			name: 'Thom Jones',
