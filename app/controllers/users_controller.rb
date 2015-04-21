27423aa36359253542c3502dfa20e4a1ab7897b3
Kristen MacFarlane <krismacfarlane@gmail.com>
2015-04-21 14:56:17 -0400

    removes the lonely binding pry

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,6 @@ class UsersController < ApplicationController
   def index
     # renders all users
     @users = User.all
-    @questionnaires = Questionnaire.all
 
     # non-specific user search
     if params[:industry]
@@ -24,7 +23,6 @@ class UsersController < ApplicationController
       search = params[:search].titleize
       @user_results = User.where("name LIKE ?", "%#{search}%")
     end
-    # binding.pry
   end
 
   def show
@@ -42,7 +40,6 @@ class UsersController < ApplicationController
     end
       # visitor = params[:visitor]
       # cur_user = @current_user.id
-      # binding.pry
   end
 
   def new
