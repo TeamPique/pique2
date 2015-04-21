ceda51cc3578a379c30fcff8977cd952105011e0
Kamari Aykes Aykes <aykeskc@gmail.com>
2015-04-21 15:05:11 -0400

    fixes user's projects' show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -27,15 +27,17 @@ class UsersController < ApplicationController
   end
 
   def show
-    if params[:id].to_i == current_user.id
-      @followed_projects = current_user.projects_followed
+    # if params[:id].to_i == current_user.id
+      # @followed_projects = current_user.projects_followed
+      @projects = Project.all
+      @project = Project.find(params[:id])
       render "profile"
-    else
-      visitor_id = User.find(params[:id]).id
-      have_visited = Visitor.where({user_id: visitor_id, visitor_id: current_user.id})
-      if have_visited.empty? == true
-        Visitor.create({user_id: visitor_id, visitor_id: current_user.id, date: Date.today})
-      end
+    # else
+      # visitor_id = User.find(params[:id]).id
+      # have_visited = Visitor.where({user_id: visitor_id, visitor_id: current_user.id})
+      # if have_visited.empty? == true
+      #   Visitor.create({user_id: visitor_id, visitor_id: current_user.id, date: Date.today})
+      # end
       @user = User.find(params[:id])
       @questionnaire = Questionnaire.find(params[:id])
       render "show"
@@ -63,7 +65,7 @@ class UsersController < ApplicationController
     redirect_to user_path(current_user)
   end
 
-end
+# end
 
 
 
