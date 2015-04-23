957d839e7e1848acc09b13b24b5894255cd73216
Kamari Aykes Aykes <aykeskc@gmail.com>
2015-04-23 10:48:56 -0400

    fixed @followed_projects

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -30,22 +30,22 @@ class UsersController < ApplicationController
   end
 
   def show
-    # if params[:id].to_i == current_user.id
-      # @followed_projects = current_user.projects_followed
+    if params[:id].to_i == current_user.id
+      @followed_projects = current_user.projects_followed
       @projects = Project.all
       @project = Project.find(params[:id])
       render "profile"
-    # else
-      # visitor_id = User.find(params[:id]).id
-      # have_visited = Visitor.where({user_id: visitor_id, visitor_id: current_user.id})
-      # if have_visited.empty? == true
-      #   Visitor.create({user_id: visitor_id, visitor_id: current_user.id, date: Date.today})
-      # end
+    else
+      visitor_id = User.find(params[:id]).id
+      have_visited = Visitor.where({user_id: visitor_id, visitor_id: current_user.id})
+      if have_visited.empty? == true
+        Visitor.create({user_id: visitor_id, visitor_id: current_user.id, date: Date.today})
+      end
       @user = User.find(params[:id])
       render "show"
     end
-      # visitor = params[:visitor]
-      # cur_user = @current_user.id
+      visitor = params[:visitor]
+      cur_user = @current_user.id
   end
 
   def new
@@ -91,5 +91,5 @@ def portfolio_params
     params.permit(:portfolio,:id)
   end
 
-
+  end
 end
